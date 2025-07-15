48394118094a106b8c707be406e4fc536ceb5152
Gabriel Gomes <gabrielgomeseleven@gmail.com>
2025-07-15 13:42:09 -0300

    Corrige caminhos relativos das imagens para GitHub Pages

--- script.js
+++ script.js
@@ -11,22 +11,22 @@ function changeTheme() {
   body.classList.toggle('dark');
 
   if (body.classList.contains('dark')) {
-    body.style.backgroundImage = '/assets/imagens/fundo_preto.svg';
-    icone.src = '/assets/imagens/icons/sun.svg';
-    logo.src = '/assets/imagens/icons/logo_branco.svg';
-    iconProjects.src = '/assets/imagens/icons/projects_branco.svg';
-    iconLanguage.src = '/assets/imagens/icons/language_branco.svg';
-    iconCollege.src = '/assets/imagens/icons/college_branco.svg';
-    iconEmail.src = '/assets/imagens/icons/email_branco.svg';
-    profilePhoto.src = '/assets/imagens/profile_preto.png';
+    body.style.backgroundImage = 'assets/imagens/fundo_preto.svg';
+    icone.src = 'assets/imagens/icons/sun.svg';
+    logo.src = 'assets/imagens/icons/logo_branco.svg';
+    iconProjects.src = 'assets/imagens/icons/projects_branco.svg';
+    iconLanguage.src = 'assets/imagens/icons/language_branco.svg';
+    iconCollege.src = 'assets/imagens/icons/college_branco.svg';
+    iconEmail.src = 'assets/imagens/icons/email_branco.svg';
+    profilePhoto.src = 'assets/imagens/profile_preto.png';
   } else {
-    icone.src = '/assets/imagens/icons/moon.svg';
-    logo.src = '/assets/imagens/icons/logo_preta.svg';
-    iconProjects.src = '/assets/imagens/icons/projects_preto.svg';
-    iconLanguage.src = '/assets/imagens/icons/language_preto.svg';
-    iconCollege.src = '/assets/imagens/icons/college_preto.svg';
-    iconEmail.src = '/assets/imagens/icons/email_preto.svg';
-    profilePhoto.src = '/assets/imagens/profile_branco.jpg';
+    icone.src = 'assets/imagens/icons/moon.svg';
+    logo.src = 'assets/imagens/icons/logo_preta.svg';
+    iconProjects.src = 'assets/imagens/icons/projects_preto.svg';
+    iconLanguage.src = 'assets/imagens/icons/language_preto.svg';
+    iconCollege.src = 'assets/imagens/icons/college_preto.svg';
+    iconEmail.src = 'assets/imagens/icons/email_preto.svg';
+    profilePhoto.src = 'assets/imagens/profile_branco.jpg';
   }
 }
 
